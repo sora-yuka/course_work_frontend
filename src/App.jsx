b6c898118ad2bac39ef42f2673ce89f00dc4340b
sora-yuka <feelingjeez@gmail.com>
2026-02-17 21:18:58 +0600

    changing paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
-import { AuthProvider } from '@infrastructure/context/AuthProvider'
-import ProtectedRoute from '@infrastructure/utils/protectedroute'
-import Login from '@presentation/pages/login/Login'
-import Home from '@presentation/pages/home/Home'
-import Dashboard from '@presentation/pages/dashboard/Dashboard'
-import Showcase from '@presentation/pages/showcase/Showcase'
-import Sold from '@presentation/pages/sold/Sold'
-import Stock from '@presentation/pages/stock/Stock'
-import Notfound from '@presentation/pages/notfound/Notfound'
+import { AuthProvider } from './infrastructure/context/AuthProvider'
+import ProtectedRoute from './infrastructure/utils/ProtectedRoute'
 
 
 export default function App() {
   return (
     <AuthProvider>
       <Routes>
-        <Route path='/login' element={ <Login/> } />
+        {/* <Route path='/login' element={ </> } /> */}
         <Route element={ <ProtectedRoute/> }>
-          <Route path='/' element={ <Home/> }>
-            <Route index element={ <Dashboard/> } />
-            <Route path='/showcase' element={ <Showcase/> } />
-            <Route path='/sold' element={ <Sold/> } />
-            <Route path='/stock' element={ <Stock/> } />
-          </Route>
+          <Route path='/' />
         </Route>
-        <Route path='*' element={ <Notfound/> } />
       </Routes>
     </AuthProvider>
   )
--- src/presentation/pages/notfound/Notfound.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Notfound(){
-  return (
-    <div>
-      Not found page. 
-    </div>
-  )
-}
